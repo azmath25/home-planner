0d79ac8a871d512989612f6c2d1d1a0a9d787104
azmath25 <234483322+azmath25@users.noreply.github.com>
2026-05-17 19:39:44 +0400

    Fix configuration for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This 'base' property is the magic line that fixes the blank white screen on GitHub Pages
-  base: './', 
+  base: './',
 })
