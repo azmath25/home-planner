7e4afa75e3d170e67f0f98b8d093bda6020c6c82
azmath25 <234483322+azmath25@users.noreply.github.com>
2026-05-17 18:56:24 +0400

    Set base property for GitHub Pages
    Add base property to fix GitHub Pages deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This 'base' property is the magic line that fixes the blank white screen on GitHub Pages
+  base: './', 
 })
